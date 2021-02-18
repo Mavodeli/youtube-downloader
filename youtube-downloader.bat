33eb45cc56c2353e103767212fd3d94c6b957f0f
Mavodeli <61018022+Mavodeli@users.noreply.github.com>
2021-02-18 01:02:12 +0100

    upload the damn file already

---
+++ youtube-downloader.bat
@@ -0,0 +1,42 @@
+@echo off
+set /p url="URL (playlist or single video): "
+echo Set Playlist URL to: %url%
+
+echo .
+:a
+echo Make sure that the folder does not exist before running the script! 
+
+set /p folder="Name of the folder you would like the playlist to be saved as: "
+
+if exist "%CD%"\"%folder%" echo "This folder already exists!" & Pause & exit
+mkdir "%CD%"\"%folder%"
+echo Created Folder "%folder%"
+
+
+xcopy "%CD%\src\ffmpeg.exe" ""%CD%"\%folder%"
+xcopy "%CD%\src\youtube-dl.exe" "%CD%\%folder%"
+xcopy "%CD%\src\libcrypto-1_1.dll" "%CD%\%folder%"
+xcopy "%CD%\src\libssl-1_1.dll" "%CD%\%folder%"
+
+
+cd "%CD%"\"%folder%"
+
+echo .
+set /p extract="Generate mp3 files from video? (Y/N): "
+
+if "%extract%" == "y" goto :xtract
+if "%extract%" == "Y" goto :xtract
+
+youtube-dl.exe -i %url%
+goto :end
+
+:xtract
+youtube-dl.exe -i -x --audio-format mp3 %url%
+
+:end
+del "ffmpeg.exe"
+del "youtube-dl.exe"
+del "libcrypto-1_1.dll"
+del "libssl-1_1.dll"
+
+pause
