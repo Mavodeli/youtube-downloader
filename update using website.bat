5bbcff2516548678d8dc4c9afd011051d365bd78
Mavodeli <mavodeli85@gmail.com>
2022-07-09 13:23:32 +0200

    remove debug instruction

--- update using website.bat
+++ update using website.bat
@@ -1,5 +1,4 @@
 FOR %%I IN (.) DO SET currentFolderName=%%~nI%%~xI
-echo %currentFolderName%
 cd ../
 powershell -command "Start-BitsTransfer -Source https://mavodeli.de/youtube-downloader/youtube-downloader.zip -Destination ytdlupdate.zip"
 powershell -command "Expand-Archive ytdlupdate.zip '%currentFolderName%' -Force"
