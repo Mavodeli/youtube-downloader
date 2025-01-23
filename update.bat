16cc1fff103b26a5e2ec1edd1a295a5ad2d9041b
Mavodeli <mavodeli85@gmail.com>
2025-01-23 21:23:09 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 FOR %%I IN (.) DO SET currentFolderName=%%~nI%%~xI
 cd ../
-powershell -command "Start-BitsTransfer -Source https://mavodeli.de/youtube-downloader/youtube-downloader.zip -Destination ytdlupdate.zip"
+powershell -command "Start-BitsTransfer -Source https://mavodeli.de/media/other/youtube-downloader.zip -Destination ytdlupdate.zip"
 powershell -command "Expand-Archive ytdlupdate.zip '%currentFolderName%' -Force"
 del ytdlupdate.zip
 exit
